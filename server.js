16c579be1b5c6693332e7d826a1bad247304a0aa
goelayush7 <129718809+goelayush7@users.noreply.github.com>
2024-02-16 23:35:00 +0530

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ connectDB();
 //rest object
 const app = express();
 
-middelwares
+// middelwares
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 app.use(cors());
